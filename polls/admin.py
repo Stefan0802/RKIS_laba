9763d338a6ac21170dd5b4ffa820af1d3cfa25c7
Stefan0802 <127651893+Stefan0802@users.noreply.github.com>
2024-11-02 14:57:20 +0700

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,14 +1,14 @@
-from django.contrib import admin  # Импортируем модуль admin из Django, который позволяет создавать интерфейс администрирования для моделей.
-from .models import Question, Choice  # Импортируем модели Question и Choice из текущего приложения.
+from django.contrib import admin  
+from .models import Question, Choice  
 
-# Определяем класс ChoiceInLine, который будет отображать варианты ответов (Choice) в виде инлайновой формы в админке.
+# Определяем класс ChoiceInLine, который будет отображать варианты ответов (Choice) в виде инлайновой формы в админке
 class ChoiceInLine(admin.TabularInline):
-    model = Choice  # Указываем, что этот инлайн будет работать с моделью Choice.
-    extra = 3  # Указываем, что по умолчанию будет отображаться 3 пустые формы для добавления новых вариантов ответов.
+    model = Choice  # Указываем, что будет работать с моделью Choice
+    extra = 3  # Указываем, что по умолчанию будет отображаться 3 пустые формы для добавления новых вариантов ответов
 
-# Определяем класс QuestionAdmin, который будет управлять отображением модели Question в админке.
+# Определяем класс QuestionAdmin, который будет управлять отображением модели Question в админке
 class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInLine]  # Указываем, что в админке для Question будут отображаться инлайновые формы ChoiceInLine.
+    inlines = [ChoiceInLine]  # Указываем, что в админке для Question будут отображаться инлайновые формы ChoiceInLine
 
-# Регистрируем модель Question и связываем ее с классом QuestionAdmin в админке.
+# Регистрируем модель Question и связываем ее с классом QuestionAdmin в админке
 admin.site.register(Question, QuestionAdmin)
